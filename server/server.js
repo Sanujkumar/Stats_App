6f97d78f03895f41444c6199927bc7530bc76c61
sanuj <sanujkumar67943@gmail.com>
2024-12-07 22:41:52 +0530

    initial file add

---
+++ server/server.js
@@ -0,0 +1,20 @@
+
+const express = require('express');
+const cors = require('cors');
+
+const apiRoutes = require('./routes/api');
+const mongoConnect = require('./db/mongoConnect');
+
+const app = express();
+
+mongoConnect();
+app.use(express.json());
+app.use(cors());
+
+app.use('/', apiRoutes);
+
+const PORT = 8080;  
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
